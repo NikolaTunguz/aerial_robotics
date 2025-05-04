6f94fe8a41cfa46420879e3a0bb5dddf6a5849d5
Joel Molina <nvmolinajoel@gmail.com>
2025-05-03 20:05:26 -0700

    testing

--- robowork_minihawk_control/scripts/final_project.py
+++ robowork_minihawk_control/scripts/final_project.py
@@ -5,23 +5,28 @@ from mavros_msgs.srv import SetMode, CommandBool
 from apriltag_ros.msg import AprilTagDetectionArray
 from mavros_msgs.msg import OverrideRCIn
 
-
 class FinalProject:
     def __init__(self):
-        #variables for storing information
         self.apriltag_data = None
         self.apriltag_detection = False
+        self.rate = rospy.Rate(10)  # 10 Hz update
 
-        #creating nodes
-        rospy.init_node('final_project_node', anonymous = True)
-        
-        #establishing services
+        rospy.init_node('final_project_node', anonymous=True)
+
         rospy.wait_for_service('/minihawk_SIM/mavros/set_mode')
         rospy.wait_for_service('/minihawk_SIM/mavros/cmd/arming')
 
+        self.apriltag_subscriber = rospy.Subscriber(
+            '/minihawk_SIM/MH_usb_camera_link_optical/tag_detections', 
+            AprilTagDetectionArray, 
+            self.apriltag_return
+        )
 
-        #calling services
-        #print('\n\n\n\n\n\n\n\n\n\n\TESTTESTTESTTEST\n\n\n\n\n\n')
+        self.rc_pub = rospy.Publisher(
+            '/minihawk_SIM/mavros/rc/override', 
+            OverrideRCIn, queue_size=10
+        )
+
         self.set_auto_mode()
         self.arm_motors()
         self.wait_for_apriltag()
@@ -32,86 +37,87 @@ class FinalProject:
         try:
             set_mode = rospy.ServiceProxy('/minihawk_SIM/mavros/set_mode', SetMode)
             set_mode(0, 'AUTO')
-
         except rospy.ServiceException as e:
-            print('Errored: ', e)
+            print('Error setting AUTO mode:', e)
 
     def arm_motors(self):
         try:
             armed = rospy.ServiceProxy('/minihawk_SIM/mavros/cmd/arming', CommandBool)
             armed(True)
         except rospy.ServiceException as e:
-            print('Errored: ', e)
+            print('Error arming motors:', e)
 
     def apriltag_return(self, message):
         if len(message.detections) > 0:
             self.apriltag_data = message.detections[0]
             self.apriltag_detection = True
-            #print('\n\nnew apriltag data\n\n')
+        else:
+            self.apriltag_data = None
+            self.apriltag_detection = False
 
-    def get_apriltag_position(self, pose):
-        while hasattr(pose, "pose"):
-            pose = pose.pose
-        return pose.position
+    def get_apriltag_offsets(self):
+        """Returns (x_offset, y_offset) if available, else None."""
+        if not self.apriltag_data:
+            return None, None
+        pose = self.apriltag_data.pose.pose.pose
+        return pose.position.x, pose.position.y
 
     def wait_for_apriltag(self):
-        apriltag_subscriber = rospy.Subscriber('/minihawk_SIM/MH_usb_camera_link_optical/tag_detections', AprilTagDetectionArray, self.apriltag_return)
-        while not self.apriltag_detection and not rospy.is_shutdown():
-            rospy.sleep(0.1)
-        rospy.sleep(5)
+        while not rospy.is_shutdown():
+            if self.apriltag_detection:
+                rospy.loginfo("AprilTag detected!")
+                break
+            self.rate.sleep()
+        rospy.sleep(1)  # short pause
 
     def finetune_position(self):
-        #setting to qloiter
         try:
             set_mode = rospy.ServiceProxy('/minihawk_SIM/mavros/set_mode', SetMode)
-            set_mode(0, 'QLOITER')  
+            set_mode(0, 'QLOITER')
         except rospy.ServiceException as e:
-            print('Errored: ', e)
+            print('Error setting QLOITER mode:', e)
 
-        publish_control = rospy.Publisher('/minihawk_SIM/mavros/rc/override', OverrideRCIn, queue_size = 10)
+        tolerance = 0.1  # You can set this lower if you want higher precision
+        max_iterations = 100
+        iteration = 0
 
-        #adjusting position
-        while True:
-            if self.apriltag_data:
-                #take data
-                apriltag_position = self.get_apriltag_position(self.apriltag_data.pose)#self.apriltag_data.pose.pose.position
-                apriltag_x_offset = apriltag_position.x
-                apriltag_y_offset = apriltag_position.y
+        while not rospy.is_shutdown() and iteration < max_iterations:
+            x_offset, y_offset = self.get_apriltag_offsets()
+            if x_offset is not None and y_offset is not None:
+                print("Offsets x: {:.3f}  y: {:.3f}".format(x_offset, y_offset))
 
-                print(apriltag_x_offset, apriltag_y_offset)
+                # Check if within tolerance
+                if abs(x_offset) < tolerance and abs(y_offset) < tolerance:
+                    print("Arrived at center of AprilTag! Offsets ~ 0")
+                    break
 
-                if abs(apriltag_x_offset) < 1 and abs(apriltag_y_offset) < 1:
-                    #break
-                    print('it tried to break')
-                
-
-                #calculate roll
-                p = 50
-                roll = max(1000, min(2000, int (1500 + p * apriltag_y_offset)))
-
-                #calculate pitch
-                p = 50
-                pitch = max(1000, min(2000, int (1500 - p * apriltag_x_offset)))
-
+                # RC calculations
+                P = 200  # gain (tune as necessary for your setup)
+                roll = int(max(1000, min(2000, 1500 + P*y_offset)))
+                pitch = int(max(1000, min(2000, 1500 - P*x_offset)))
                 throttle = 1500
                 yaw = 1500
+                rc_override = OverrideRCIn()
+                rc_override.channels = [roll, pitch, throttle, yaw] + [0]*14
 
-                control = OverrideRCIn()
-                control.channels = [roll, pitch, throttle, yaw, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                self.rc_pub.publish(rc_override)
+            else:
+                print("No AprilTag detected, holding position.")
 
-                publish_control.publish(control)
-
-                rospy.sleep(0.5)
-
+            self.rate.sleep()
+            iteration += 1
 
     def set_land_mode(self):
         try:
             set_mode = rospy.ServiceProxy('/minihawk_SIM/mavros/set_mode', SetMode)
             set_mode(0, 'QLAND')
-
+            print("Landing...")
         except rospy.ServiceException as e:
-            print('Errored: ', e)
+            print('Error setting QLAND mode:', e)
 
 
 if __name__ == '__main__':
-    program = FinalProject()
+    try:
+        FinalProject()
+    except rospy.ROSInterruptException:
+        pass
